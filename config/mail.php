1d53c1aef983e594e676f953dce5349120c83f44
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-05-10 15:44:32 +0800

    Updated config

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_SENDER'), 'name' => 'Fuck Orbital'],
 
     /*
     |--------------------------------------------------------------------------
