779701fc38548cc2fa6a63631f004e3956b37afc
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-05-10 17:46:43 +0800

    update visuals

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,8 @@
   <header>
   <h1>Dear Orbital:
   <a href="{{ action('FuckController@create') }}" class="finger">🖕</a></h1>
-    <p>An Orbital project that meets the intermediate (Gemini) achivement level, created in one day.
-      <a href="https://github.com/ZhangYiJiang/fuck-orbital">Fork us on GitHub!</a></p>
+    <p>An Orbital project that meets the intermediate (Gemini) achievement level, created in one day.
+      <a href="https://github.com/ZhangYiJiang/fuck-orbital">Read the project log or fork us on GitHub!</a></p>
   </header>
 @endsection
 
